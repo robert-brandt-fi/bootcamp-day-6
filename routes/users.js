4da1fbff351ae3eebd024970295758b2e2b70620
Robert Brandt <robert.brandt@hotmail.fi>
2021-10-19 10:09:11 +0300

    Add new functions to users.js

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/first', function(request, response) {
+  console.log("First function called.");
+  response.send("The First has been summmoned!");
+});
+
+router.get('/second:firstName', function(request, response) {
+  console.log("Second function called. " + request.params.firstName);
+  response.send("The Second has been summmoned! " + request.params.firstName);
+});
+
 module.exports = router;
