2ba746f2668713e8c6a7e2e397c416f7f6301a56
Robert Brandt <robert.brandt@hotmail.fi>
2021-10-19 14:40:29 +0300

    Finish initial book routine

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var reload = require('reload');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bookRouter = require('./routes/book');
 
 var app = express();
+
+const helmet = require('helmet');
+const cors = require('cors');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,19 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(helmet());
+app.use(cors());
+
+app.use('/book', bookRouter);
 
 module.exports = app;
-
-// Reload code here
-reload(app).then(function (reloadReturned) {
-    // reloadReturned is documented in the returns API in the README
-  
-    // Reload started, start web server
-    server.listen(app.get('port'), function () {
-      console.log('Web server listening on port ' + app.get('port'))
-    })
-  }).catch(function (err) {
-    console.error('Reload could not start, could not start server/sample app', err)
-  })
